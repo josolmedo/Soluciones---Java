6fbda83ac022e64cfe20fe9c2dd97ba37e026ee4
José Olmedo <116040147+josolmedo@users.noreply.github.com>
2024-06-03 21:43:22 -0600

    Add files via upload

---
+++ CaracteresEspeciales.java
@@ -0,0 +1,19 @@
+//Esste es un comentario en Java
+//La clase publica principal debe llamarse igual a como vamos a guardar el programa
+// Las clases inician con mayúscula y las variables con minúscula
+// Al final de cada sentencia debemos agregar ;
+public class CaracteresEspeciales {
+    public static void main(String args[]){
+        
+     var nombre="José";
+     
+     System.out.println("\nNueva línea:\n"+nombre); //Salto de línea es con antidiagonal + n (\n)
+     System.out.println("Tabulador:\t"+nombre); //Tabular es con antidiagonal + t (\t)
+     System.out.println("Retroceso: \b"+nombre+"\n"); //Para retroceso es antidiagonal +b (\b)
+     System.out.println("Comilla simple: \'"+nombre+"\'"); //Imprimir el nombre en comillas simples
+     System.out.println("Comillas dobles: \""+nombre+"\"\n"); //Imprimir comillas dobles
+                 //Video 13
+
+    }
+    
+}
