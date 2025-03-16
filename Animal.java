9a6b05463ea28c729a0e4eca633ee1bf5ff21897
José Olmedo <116040147+josolmedo@users.noreply.github.com>
2025-03-15 23:21:17 -0600

    Add files via upload
    ----Compilar con javac Main.java------
    ----Ejecutar con java Main--------

---
+++ Animal.java
@@ -0,0 +1,18 @@
+abstract class Animal{ //Clase abstracta
+
+	/***Método abstracto***/
+	abstract void hacerSonido();
+
+	abstract void dormir();
+
+	public void cazar(){
+		System.out.println("Iré a cazar");
+	}
+	
+	
+
+
+	
+
+
+}
